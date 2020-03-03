63177d6318020b91bced5c4f050993c74fa7db96
Tiny <dgramirez@student.fullsail.edu>
2020-03-03 01:42:35 -0500

    Did Addition Checks

--- src/unit_tests/ut_vec4.cpp
+++ src/unit_tests/ut_vec4.cpp
@@ -103,13 +103,13 @@ TEST_CASE("Contructors and Assignments", "[vec4], [operator=]") {
 
 }
 
-TEST_CASE("Equality Checks", "[vec4], [operator=]") {
+TEST_CASE("Equality Checks", "[IsZero], [IsEqual], [operator==] [operator!=]") {
 
 	const vec4 ImZero;
 	const vec4 ImNotZero(rand_num(), rand_num(), rand_num(), rand_num());
 
 	SECTION("IsZero Equality Check", "[IsZero]") {
-		//Default Constructor Test
+		//Vector Member Variable Test
 		vec4 zero;
 		CHECK(zero.IsZero());
 
@@ -126,7 +126,138 @@ TEST_CASE("Equality Checks", "[vec4], [operator=]") {
 	}
 
 	SECTION("IsEqual Equality Check", "[IsEqual], [operator==]") {
+		//Vector Member Function: (Vector)
+		vec4 NotZeroV2 = ImNotZero;
+		CHECK(NotZeroV2.IsEqual(ImNotZero));
 
+		//Vector Member Function: (Float Pointer)
+		float fp[4] = { ImNotZero.x, ImNotZero.y, ImNotZero.z, ImNotZero.w };
+		CHECK(NotZeroV2.IsEqual(fp));
+
+		//Vector Member Funciton: (m128)
+		__m128 m128 = _mm_set_ps(ImNotZero.w, ImNotZero.z, ImNotZero.y, ImNotZero.x);
+		CHECK(NotZeroV2.IsEqual(m128));
+
+		//Static Check: Vector vs Vector
+		CHECK(vec4::IsEqual(NotZeroV2, ImNotZero));
+
+		//Static Check: Vector vs Float Pointer
+		CHECK(vec4::IsEqual(NotZeroV2, fp));
+		CHECK(vec4::IsEqual(fp, NotZeroV2));
+
+		//Static Check: Vector vs m128
+		CHECK(vec4::IsEqual(NotZeroV2, m128));
+		CHECK(vec4::IsEqual(m128, NotZeroV2));
+
+		//Equality Operator Test
+		CHECK(NotZeroV2 == ImNotZero);
+		CHECK(NotZeroV2 == fp);
+		CHECK(fp == ImNotZero);
+		CHECK(NotZeroV2 == m128);
+		CHECK(m128 == ImNotZero);
 	}
 
+	SECTION("Inequality Operator Check", "[operator!=]") {
+		//Create Random Vectors
+		vec4 RandomVec1(rand_num(), rand_num(),rand_num(),rand_num());
+		vec4 RandomVec2(rand_num(), rand_num(),rand_num(),rand_num());
+		float RandomFP[4] = { rand_num(), rand_num(),rand_num(),rand_num() };
+		__m128 RandomM128 = _mm_set_ps(rand_num(), rand_num(),rand_num(),rand_num());
+
+		//Inequality Operator Test
+		CHECK(RandomVec1 != RandomVec2);
+		CHECK(RandomVec1 != RandomFP);
+		CHECK(RandomFP != RandomVec2);
+		CHECK(RandomVec1 != RandomM128);
+		CHECK(RandomM128 != RandomVec2);
+	}
+
+}
+
+TEST_CASE("Basic Vector Math", "[Add], [Sub], [Mul], [Div]")
+{
+	//PreReq Check
+	vec4 chk1(0, 1, 2, 3);
+	__m128 chk2 = _mm_set_ps(3, 2, 1, 0);
+
+	//Random Values
+	vec4 randVec(19402.85160f, 94962.10349f, 316.59856f, 86832.32890f);
+	vec4 randVec2(36055.11004f, 47454.84500f, 69497.31268f, 624.19770f);
+	float randFP[4] = { 4525.92146f, 64090.32973f, 40040.35343f, 119.59643f };
+	__m128 randM128 = _mm_set_ps(79553.85881f, 97658.71592f, 23392.63518f, 1572.67638f);
+	
+	//Answer Array Setup
+	vec4 answer[10] = {};
+
+	SECTION("Addition", "[Add], [Operator+], [Operator+=]") {
+		//Setup Answers for this section
+//		answer[0].m128 = _mm_set_ps(87456.5266f, 69813.91124f, 142416.94849f, 55457.96164f);
+		
+		//Setup Copy Vector to use
+		vec4 myVec = randVec;
+
+		//Member Variable Add: Vector vs Vector ----------------------
+		myVec.Add(randVec2);
+		CHECK(myVec == answer[0]);
+
+		//Member Variable Add: Vector vs Float Pointer
+		myVec.Add(randFP);
+		CHECK(myVec == answer[1]);
+
+		//Member Variable Add: Vector vs m128
+		myVec.Add(randM128);
+		CHECK(myVec == answer[2]);
+
+		//Member Operator+= Add: Vector vs Vector ---------------------
+		myVec += randVec;
+		CHECK(myVec == answer[3]);
+
+		//Member Operator+= Add: Vector vs Float Pointer
+		myVec += randFP;
+		CHECK(myVec == answer[4]);
+
+		//Member Operator+= Add: Vector vs m128
+		myVec += randM128;
+		CHECK(myVec == answer[5]);
+
+		//Static Add: Vector vs Vector ---------------------------------
+		myVec = vec4::Add(randVec, randVec2);
+		CHECK(myVec == answer[0]);
+
+		//Static Add: Vector vs FP
+		myVec = vec4::Add(randVec, randFP);
+		CHECK(myVec == answer[6]);
+
+		//Static Add: FP vs Vector
+		myVec = vec4::Add(randFP, randVec2);
+		CHECK(myVec == answer[7]);
+
+		//Static Add: Vector vs m128
+		myVec = vec4::Add(randVec, randM128);
+		CHECK(myVec == answer[8]);
+
+		//Static Add: m128 vs Vector
+		myVec = vec4::Add(randM128, randVec2);
+		CHECK(myVec == answer[9]);
+
+		//Operator+ Add: Vector vs Vector ------------------------------
+		myVec = randVec + randVec2;
+		CHECK(myVec == answer[0]);
+
+		//Operator+ Add: Vector vs FP
+		myVec = randVec + randFP;
+		CHECK(myVec == answer[6]);
+
+		//Operator+ Add: FP vs Vector
+		myVec = randFP + randVec2;
+		CHECK(myVec == answer[7]);
+
+		//Operator+ Add: Vector vs m128
+		myVec = randVec + randM128;
+		CHECK(myVec == answer[8]);
+
+		//Operator+ Add: m128 vs Vector
+		myVec = randM128 + randVec2;
+		CHECK(myVec == answer[9]);
+	}
 }
